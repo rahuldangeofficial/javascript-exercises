0ec1a0a8d204d36b348c02a10e3069eabfd99096
rahuldangeofficial <rahuldangeofficial@gmail.com>
2022-09-27 10:23:26 +0530

    add 08 task solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
 const add = function() {
-	
+  for(let i=1;i<arguments.length;i++){
+    arguments[0]+=arguments[i];
+  }
+  return arguments[0];
 };
 
 const subtract = function() {
-	
+  for(let i=1;i<arguments.length;i++){
+    arguments[0]-=arguments[i];
+  }
+  return arguments[0];
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if(arr[0]===undefined){
+    arr[0]=0;
+  }else{
+    for(let i=1;i<arr.length;i++){
+      arr[0]+=arr[i];
+    }
+  }
+  return  arr[0];
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr[0]===undefined){
+    arr[0]=0;
+  }else{
+    for(let i=1;i<arr.length;i++){
+      arr[0]*=arr[i];
+    }
+  }
+  return  arr[0];
 };
 
 const power = function() {
-	
+	for(let i=1;i<arguments.length;i++){
+    arguments[0]**=arguments[i];
+  }
+  return arguments[0];
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result=1;
+  for(let i=1;i<=num;i++){
+    result*=i;
+  }
+  return result;
 };
 
 // Do not edit below this line
